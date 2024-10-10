eb21950e86752169b7693adb9070ec1f6e25b031
JameKilleR <149460750+JameKilleR@users.noreply.github.com>
2024-10-10 08:20:46 +0000

    fresh product from api

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,5 +1,6 @@
 import './Shop.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Item(props) {
     return (
@@ -13,14 +14,18 @@ function Item(props) {
 }
 
 export default function Shop() {
-    const products = [
-        { id: 0, name: "Notebook Acer Swift", price: 45900, img: "https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg" },
-        { id: 1, name: "Notebook Asus Vivo", price: 19900, img: "https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg" },
-        { id: 2, name: "Notebook Lenovo Ideapad", price: 32900, img: "https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg" },
-        { id: 3, name: "Notebook MSI Prestige", price: 54900, img: "https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg" },
-        { id: 4, name: "Notebook DELL XPS", price: 99900, img: "https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg" },
-        { id: 5, name: "Notebook HP Envy", price: 46900, img: "https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg" }
-    ];
+    const [products,setProducts]=useState([]);
+    const URL="https://redesigned-giggle-r444795rpp5q2w5p-5000.app.github.dev"
+    useEffect(()=>{
+        axios.get(URL+"/api/products")
+        .then(response=>{
+            setProducts(response.data)
+        })
+        .catch(error=>{
+            console.log("error")
+        })
+    }
+    ,[]);
 
     const [cart, setCart] = useState([]);
 
@@ -44,7 +49,7 @@ export default function Shop() {
     const productList = products.map(item => <Item key={item.id} {...item} callback={addCart} />);
     const cartList = cart.map((item, index) => (
         <li key={index}>
-            {item.id} {item.name} {item.price}
+            {item.id+1} {item.name} {item.price}
             <button onClick={() => remove(index)}>Delete</button>
 
         </li>
